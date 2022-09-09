d3250d3b93df90e3e95fee0276abc5c734cac82d
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2022-09-09 10:43:24 -0500

    Add remaining code to solve magic square with CSP

--- 6. CSP/magic_square.py
+++ 6. CSP/magic_square.py
@@ -2,12 +2,15 @@ from typing import NamedTuple, Optional
 from csp import CSP, Constraint
 
 
+Grid = list[list[int]]
+
+
 class GridLocation(NamedTuple):
     row: int
     column: int
 
 
-def check_square(square: list[list[int]]) -> bool:
+def check_square(square: Grid) -> bool:
     ((a, b, c),
      (d, e, f),
      (g, h, i)) = square
@@ -28,4 +31,32 @@ class MagicPuzzleConstraint(Constraint[int, GridLocation]):
         self.variables: list[int] = variables
 
     def satisfied(self, assignment: dict[int, GridLocation]) -> bool:
-        ...
+        if len(set(assignment.values())) != len(assignment):
+            return False
+        if len(assignment) < 9:
+            return True
+        grid: Grid = [[0, 0, 0],
+                      [0, 0, 0],
+                      [0, 0, 0]]
+        for key, value in assignment.items():
+            row, column = value
+            grid[row][column] = key
+        return check_square(grid)
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    variables: list[int] = list(range(1, 10))
+    all_grid_locations: list[GridLocation] = [GridLocation(r, c)
+                                              for r in range(3)
+                                              for c in range(3)]
+    domains: dict[int, list[GridLocation]] = {
+        var: all_grid_locations for var in variables}
+    csp: CSP[int, GridLocation] = CSP(variables, domains)
+    csp.add_constraint(MagicPuzzleConstraint(variables))
+    solution: Optional[dict[int, GridLocation]] = csp.backtracking_search()
+
+    if solution is None:
+        print('No solution found')
+    else:
+        pprint(solution)
